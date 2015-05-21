c32cdc16b56e494274dcf1beb5e1217fc84faecb
Endiny <endiny@icloud.com>
2015-05-21 12:47:22 +0300

    empty init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on May 21, 2015, 12:44 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+
+    return 0;
+}
+
